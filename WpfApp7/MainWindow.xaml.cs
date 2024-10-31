9a32a17ebd279c32b001d43d742d8240688bfb41
SatinEI <SatinEI@310D-11>
2024-10-31 10:18:56 +0700

    Добавьте файлы проекта.

---
+++ WpfApp7/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            StartPulsingAnimation();
+        }
+        private void StartPulsingAnimation()
+        {
+
+            DoubleAnimation innerAnimation = new DoubleAnimation
+            {
+                From = 0.0,
+                To = 0.5,
+                Duration = TimeSpan.FromSeconds(1),
+                AutoReverse = true,
+                RepeatBehavior = RepeatBehavior.Forever
+            };
+
+
+            DoubleAnimation outerAnimation = new DoubleAnimation
+            {
+                From = 0.5,
+                To = 1,
+                Duration = TimeSpan.FromSeconds(1),
+                AutoReverse = true,
+                RepeatBehavior = RepeatBehavior.Forever
+            };
+
+
+            InnerStop.BeginAnimation(GradientStop.OffsetProperty, innerAnimation);
+            OuterStop.BeginAnimation(GradientStop.OffsetProperty, outerAnimation);
+        }
+    }
+}
